98e02bc3745393b1545eb6d89d669664e1245aa6
Kyle Trahan <ktrahan2@gmail.com>
2020-12-09 15:19:19 -0700

    animation kind of working

--- src/Animations/Anime.js
+++ src/Animations/Anime.js
@@ -1,30 +1,30 @@
-let path = anime.path('#path1');
-let path2 = anime.path('#path2')
-let path3 = anime.path('#path3')
+// let path = anime.path('#path1');
+// let path2 = anime.path('#path2')
+// let path3 = anime.path('#path3')
 
-anime({
-  targets: '#butterfly',
-  translateX: path('x'),
-  translateY: path('y'),
-  easing: 'easeInCubic',
-  duration: 10000,
-  loop: true
-});
+// export const path1 = anime({
+//   targets: '#butterfly',
+//   translateX: path('x'),
+//   translateY: path('y'),
+//   easing: 'easeInCubic',
+//   duration: 10000,
+//   loop: true
+// });
 
-anime({
-    targets: '#eel',
-    translateX: path2('x'),
-    translateY: path2('y'),
-    easing: 'linear',
-    duration: 10000,
-    loop: true
-  });
+// anime({
+//     targets: '#eel',
+//     translateX: path2('x'),
+//     translateY: path2('y'),
+//     easing: 'linear',
+//     duration: 10000,
+//     loop: true
+//   });
 
-anime({
-    targets: '#shark',
-    translateX: path3('x'),
-    translateY: path3('y'),
-    easing: 'linear',
-    duration: 5000,
-    loop: true
-  });
+// anime({
+//     targets: '#shark',
+//     translateX: path3('x'),
+//     translateY: path3('y'),
+//     easing: 'linear',
+//     duration: 5000,
+//     loop: true
+//   });
